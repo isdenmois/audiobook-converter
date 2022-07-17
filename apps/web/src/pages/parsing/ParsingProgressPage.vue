c019460a894a9529662846df94117ebca89babab
Denis Moiseev <isdenmois@gmail.com>
2022-07-17 22:33:01 +0300

    Parsing page redesign

--- apps/web/src/pages/parsing/ParsingProgressPage.vue
+++ apps/web/src/pages/parsing/ParsingProgressPage.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { useStore } from '@nanostores/vue'
 import { $parserProgress } from 'entities/media-parser'
+import { Spinner } from 'shared/ui'
 
 const parserProgress = useStore($parserProgress)
 </script>
 
 <template>
-  <div>Parsing ({{ parserProgress?.parsed }} / {{ parserProgress?.total }})...</div>
+  <div class="flex flex-1 items-center justify-center">
+    <Spinner />
+  </div>
 </template>
---
+++ apps/web/src/shared/ui/Spinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<style>
+.spinner {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+
+.spinner:after {
+  content: ' ';
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid var(--primary);
+  border-color: var(--primary) transparent var(--primary) transparent;
+  animation: spinner 1.2s linear infinite;
+}
+
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
