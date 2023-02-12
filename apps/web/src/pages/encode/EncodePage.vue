fa48cc5948c4eda8811341d1f4337c04fb7b6912
Denis Moiseev <isdenmois@gmail.com>
2023-02-12 10:37:27 +0300

    refactor: progress component

--- apps/web/src/pages/encode/EncodePage.vue
+++ apps/web/src/pages/encode/EncodePage.vue
@@ -2,7 +2,7 @@
 import { useStore } from '@nanostores/vue'
 import { currentBook$, progress$ } from 'features/encode'
 import { $books, BookItem } from 'entities/audiobook'
-import { Card, Cover } from 'shared/ui'
+import { Card, Cover, Progress } from 'shared/ui'
 </script>
 
 <script setup lang="ts">
@@ -25,31 +25,12 @@ const progress = useStore(progress$)
 
         <div class="flex flex-col flex-1">
           <div>{{ currentBook.title }}</div>
-          <progress class="mt-2" max="100" :value="progress" />
+          <div class="mt-2">
+            <Progress :value="progress" />
+          </div>
           <div class="mt-1 text-xs">Converting: {{ Math.round(progress) }}%</div>
         </div>
       </div>
     </Card>
   </div>
 </template>
-
-<style scoped>
-progress {
-  height: 16px;
-  appearance: none;
-  width: 100%;
-  border-radius: 4px;
-  overflow: hidden;
-  color: red;
-  background-color: red;
-}
-
-progress::-webkit-progress-bar {
-  background-color: #e3ebfe;
-}
-
-progress::-webkit-progress-value {
-  background-color: #4484ff;
-  border-radius: 4px;
-}
-</style>
---
+++ apps/web/src/shared/ui/Progress.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{ value: number }>()
+</script>
+
+<template>
+  <progress max="100" :value="value" />
+</template>
+
+<style scoped>
+progress {
+  height: 16px;
+  appearance: none;
+  width: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+  color: red;
+  background-color: red;
+}
+
+progress::-webkit-progress-bar {
+  background-color: #e3ebfe;
+}
+
+progress::-webkit-progress-value {
+  background-color: #4484ff;
+  border-radius: 4px;
+}
+</style>
